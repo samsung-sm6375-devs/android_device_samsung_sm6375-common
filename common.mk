00f12dbf8fd686a38647a96a4754bd2e0d2dd498
Haky86 <hakamelassouad@gmail.com>
2025-01-13 00:41:48 +0100

    sm6375-common: Import perf configs
    * Taken from a23xqnsxx-user 14 UP1A.231005.007 A236BXXU9EXK1 release-keys.
    
    Change-Id: I2009f1b25e779372324f0d425b3d0073a7104b57

--- common.mk
+++ common.mk
@@ -183,6 +183,15 @@ PRODUCT_PACKAGES += \
 DEVICE_PACKAGE_OVERLAYS += \
     device/samsung/qcom-common/overlay \
 
+# Perf
+PRODUCT_COPY_FILES += \
+    $(COMMON_PATH)/configs/perf/commonresourceconfigs.xml:$(TARGET_COPY_OUT_VENDOR)/etc/perf/commonresourceconfigs.xml \
+    $(COMMON_PATH)/configs/perf/commonsysnodesconfigs.xml:$(TARGET_COPY_OUT_VENDOR)/etc/perf/commonsysnodesconfigs.xml \
+    $(COMMON_PATH)/configs/perf/perfboostsconfig.xml:$(TARGET_COPY_OUT_VENDOR)/etc/perf/perfboostsconfig.xml \
+    $(COMMON_PATH)/configs/perf/perfconfigstore.xml:$(TARGET_COPY_OUT_VENDOR)/etc/perf/perfconfigstore.xml \
+    $(COMMON_PATH)/configs/perf/targetconfig.xml:$(TARGET_COPY_OUT_VENDOR)/etc/perf/targetconfig.xml \
+    $(COMMON_PATH)/configs/perf/targetresourceconfigs.xml:$(TARGET_COPY_OUT_VENDOR)/etc/perf/targetresourceconfigs.xml \
+
 # Ramdisk
 PRODUCT_PACKAGES += \
     fstab.qcom \
