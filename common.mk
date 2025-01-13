8e8986aeb081897e418895c643acb1e48a380d47
Artem Borisov <dedsa2002@gmail.com>
2025-01-13 11:57:08 +0100

    sm6375-common: overlay: Enable camera api v2
    * It's quite ugly, but we want a functional camera until HAL1 issues
    will be sorted out.
    
    Change-Id: I896bc9d9bbb1d4ea95fc34e398d9b1cf5618fae6

--- common.mk
+++ common.mk
@@ -95,6 +95,7 @@ PRODUCT_PACKAGES += \
     camera.device@3.3-impl \
     camera.device@3.4-impl \
     camera.device@3.5-impl \
+    Snap \
 
 # Charger
 PRODUCT_PACKAGES += \
@@ -187,6 +188,7 @@ DEVICE_PACKAGE_OVERLAYS += \
 
 PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += \
     $(COMMON_PATH)/overlay-lineage/lineage-sdk \
+    $(COMMON_PATH)/overlay-lineage/packages/apps/Snap \
 
 PRODUCT_ENFORCE_RRO_TARGETS := *
 
