96283f2396ba0b6952683f87ea5d1182af582358
Pawin Vongmasa <pawin@google.com>
2025-01-14 15:27:48 +0100

    sm6375-common: Enable Codec2
    Drop framework_manifest.xml part as the hal is already parsed.
    Don't bring in the c2 xml files as CAF squashed the c2
    support in the already existing media codecs.
    
    Change-Id: Ibe051f4b19c2dbb0a208e751a3b7a332a329b447

--- common.mk
+++ common.mk
@@ -276,6 +276,7 @@ PRODUCT_PACKAGES += \
     libcodec2_hidl@1.1.vendor \
     libcodec2_vndk \
     libcodec2_vndk.vendor \
+    libsfplugin_ccodec \
 
 PRODUCT_COPY_FILES += \
     $(COMMON_PATH)/configs/media/media_blair/video_system_specs.json:$(TARGET_COPY_OUT_VENDOR)/etc/media_blair/video_system_specs.json \
