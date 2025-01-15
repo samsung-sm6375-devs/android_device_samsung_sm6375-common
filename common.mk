1b85111a460ec0611a3060277db3316db12f0f7b
Tim Zimmermann <tim@linux4.de>
2025-01-15 14:28:55 +0100

    sm6375-common: Switch to Samsung's usb gadget HAL
    * Built from hardware/samsung.
    
    Change-Id: Ia9a801a38928348348e9d6b2853080a00983ccb0

--- common.mk
+++ common.mk
@@ -568,7 +568,8 @@ PRODUCT_PACKAGES += \
 # USB
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.3-service.dual_role_usb \
-    android.hardware.usb.gadget@1.0-service-qti \
+    android.hardware.usb-service.samsung \
+    android.hardware.usb.gadget-service.samsung \
     android.hardware.usb@1.0.vendor \
     android.hardware.usb@1.1.vendor \
 
